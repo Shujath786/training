b0f4d82c8a5bb0590e1d3f677347509254aba6f1
Shujath <shujath786@gmail.com>
2024-02-26 23:04:01 -0500

    first commit

---
+++ src/gitPractice/TestOne.java
@@ -0,0 +1,11 @@
+package gitPractice;
+
+public class TestOne {
+
+	public static void main(String[] args) {
+		System.out.println("This is the first test");
+		
+
+	}
+
+}
---
+++ src/gitPractice/Testtwo.java
@@ -0,0 +1,10 @@
+package gitPractice;
+
+public class Testtwo {
+
+	public static void main(String[] args) {
+		System.out.println("This is the second test");
+
+	}
+
+}
